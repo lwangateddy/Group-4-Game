08bbb45a5de8c380c13fc71d7b5864747e1ab387
lwangateddy <lwangateddy8277@gmail.com>
2017-07-27 12:14:53 +0300

    updated changes

--- gaming.js
+++ gaming.js
@@ -20,7 +20,7 @@ function setup() {
 }
 
 function draw (){
-  background(bg);
+  background(0);
   image(sprite,spriteXpos,spriteYpos,60,60);
   //image(sprite1,sprite1Xpos,sprite1Ypos,100,100);
 
@@ -32,83 +32,80 @@ function draw (){
   line(0,y,100,y);
   strokeWeight(10);
 
-  // y=y-1;
-  // if(y<0){
-  //   y=height;
-  // }
-  // line(150,y,250,y);
-  // strokeWeight(10);
+  y=y-1;
+  if(y<0){
+    y=height;
+  }
+  line(150,y,250,y);
+  strokeWeight(10);
 
 
 
-//   y=y-1;
-//   if(y<0){
-//     y=height;
-//   }
-//   line(350,y,750,y);
-//   strokeWeight(10);
-//
-//   y=y-1;
-//   if(y<0){
-//     y=height;
-//   }
-//   line(300,y-100,400,y-100);
-//   strokeWeight(10);
-//
-//   y=y-1;
-//   if(y<0){
-//     y=height;
-//   }
-//   line(0,y-100,250,y-100);
-//   strokeWeight(10);
-//
-//   y=y-1;
-//   if(y<0){
-//     y=height;
-//   }
-//   line(500,y-100,950,y-100);
-//   strokeWeight(10);
-//
-//   y=y-1;
-//   if(y<0){
-//     y=height;
-//   }
-//   line(400,y-150,750,y-150);
-//   strokeWeight(10);
-//
-//   y=y-1;
-//   if(y<0){
-//     y=height;
-//   }
-//   line(500,y-50,600,y-50);
-//   strokeWeight(10);
-//
-//   y=y-1;
-//   if(y<0){
-//     y=height;
-//   }
-//   line(0,y-50,100,y-50);
-//   strokeWeight(10);
-//
-// if (line <= 0){
-//   line= 500;
-// }
-//   yPos = yPos - 1;
-// if (yPos < 0) {
-//   yPos = width;
-// }
+  y=y-1;
+  if(y<0){
+    y=height;
+  }
+  line(350,y,750,y);
+  strokeWeight(10);
+
+  y=y-1;
+  if(y<0){
+    y=height;
+  }
+  line(300,y-100,400,y-100);
+  strokeWeight(10);
+
+  y=y-1;
+  if(y<0){
+    y=height;
+  }
+  line(0,y-100,250,y-100);
+  strokeWeight(10);
+
+  y=y-1;
+  if(y<0){
+    y=height;
+  }
+  line(500,y-100,950,y-100);
+  strokeWeight(10);
+
+  y=y-1;
+  if(y<0){
+    y=height;
+  }
+  line(400,y-150,750,y-150);
+  strokeWeight(10);
+
+  y=y-1;
+  if(y<0){
+    y=height;
+  }
+  line(500,y-50,600,y-50);
+  strokeWeight(10);
+
+  y=y-1;
+  if(y<0){
+    y=height;
+  }
+  line(0,y-50,100,y-50);
+  strokeWeight(10);
+
+if (line <= 0){
+  line= 500;
+}
+
 //rect(0, yPos, 60, yPos);
 //strokeWeight(20);
 //fill(255,0,100);
 
 
-
-
-spriteYpos -=1;
-if(spriteYpos<0){
-  spriteYpos=height;
-}
-image(sprite,spriteYpos-50,100);
+//
+//
+// spriteYpos -=1;
+// if(spriteYpos<0){
+//   spriteYpos=height;
+// }
+// image(sprite,spriteYpos-50,100);
 // strokeWeight(10);
 
 
@@ -127,20 +124,20 @@ if (keyIsDown(UP_ARROW)){
       spriteXpos+=10;
 }
 
-// if (sprite){
-//
-// }
+if (sprite){
 
-// for(var i=10; i < 20; i+=10){
-//   // var lineYpos = random(40);
-//   // line(i,lineYpos, 50,50);
-// }
-// y=y-1;
-// if(y<0){
-//   y=height;
-// }
-//line(150,y,250,y);
-//strokeWeight(10);
+}
+
+for(var i=10; i < 20; i+=10){
+  // var lineYpos = random(40);
+  // line(i,lineYpos, 50,50);
+}
+y=y-1;
+if(y<0){
+  y=height;
+}
+line(150,y,250,y);
+strokeWeight(10);
 
 
 y=y-1;
